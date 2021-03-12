e799bf5506405eddcddd989d09c22307f2595bde
dansilvaUT <dan13silv@gmail.com>
2021-03-11 16:28:52 -0800

    DB connected and starter tables

---
+++ db/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(300) NOT NULL,
+    profile_pic TEXT,
+    date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE IF NOT EXISTS chat_group (
+    group_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS chat (
+    chat_id SERIAL PRIMARY KEY,
+    group_id INT REFERENCES chat_group(group_id),
+    user_id INT REFERENCES users(user_id),
+    message TEXT
+)
--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const express = require('express'),
     massive = require('massive'),
-    { SERVER_PORT } = process.env,
+    { SERVER_PORT, CONNECTION_STRING } = process.env,
     app = express();
 
 
 app.use(express.json());
+
+//CONNECT TO OUR DATABASE
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then(db => {
+    app.set('db', db);
+    console.log('Message Box DB connected');
+}).catch(err => console.log(`Error connected to DB: ${err.message}`));
+
 
 app.listen(SERVER_PORT, console.log(`Message Box Listening on ${SERVER_PORT}`));
 
