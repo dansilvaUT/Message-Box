0cd80288b2093dc1dfbe020c99d348d979c67bb4
dansilvaUT <dan13silv@gmail.com>
2021-03-18 21:57:29 -0700

    Need to fix issue with button fireing on every start chat

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,8 +1,14 @@
-const Chat = () => {
+import ChatControls from './ChatControls/ChatControls';
+import Submit from '../Buttons/Submit';
+
+const Chat = props => {
+    const { goBack } = props.history;
+    console.log('chat controls', props)
     return (
-        <>
-            Chat Component
-        </>
+        <section className='chat-container'>
+            <Submit text='Close Chat' func={goBack} />
+            <ChatControls />
+        </section>
     )
 }
 
---
+++ src/Components/Chat/ChatControls/ChatControls.js
@@ -0,0 +1,11 @@
+import Submit from '../../Buttons/Submit';
+
+const ChatControl = () => {
+    return (
+        <section>
+            <Submit text='Send' />
+        </section>
+    )
+}
+
+export default ChatControl;
