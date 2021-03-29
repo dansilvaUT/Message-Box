fafa1ed703461a3f1a322c57b8730255b8d59ca1
dansilvaUT <dan13silv@gmail.com>
2021-03-29 16:09:00 -0700

    routed to new page when chat begins

---
+++ src/Components/Chat/Chats.js
@@ -0,0 +1,16 @@
+import ChatControls from './ChatControls/ChatControls';
+
+import './Chats.scss';
+
+const Chats = () => {
+    return (
+        <section>
+            <div className="messages-container">
+
+            </div>
+            <ChatControls />
+        </section>
+    )
+}
+
+export default Chats;
--- src/Components/Dashboard/Users/User/User.js
+++ src/Components/Dashboard/Users/User/User.js
@@ -1,18 +1,13 @@
-import Chat from '../../../Chat/Chat';
-import { Link, BrowserRouter } from 'react-router-dom';
-import { ModalContainer, ModalRoute } from 'react-router-modal';
+import { Link,} from 'react-router-dom';
 
 const User = ({ user, url }) => {
     // console.log('user', url)
     return (
         <article>
             <img src={user.profile_pic} alt={user.username} />
-            <p>{user.username}</p>
-            <BrowserRouter>
-                <Link to={`${url}/chat/${user.user_id}`}>show foo</Link>
-                <ModalRoute component={Chat} path={`${url}/chat/:id`} className='test-modal test-modal-foo' />
-                <ModalContainer />
-            </BrowserRouter>
+            <Link to={`/chat/${user.user_id}`}>
+                Chat with {user.username}
+            </Link>
         </article>
     )
 }
