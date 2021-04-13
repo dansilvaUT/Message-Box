587d64774a934ac1e8655c3423ec542396b28186
dansilvaUT <dan13silv@gmail.com>
2021-04-12 22:45:11 -0700

    need to fix and correctly implement redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,24 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import userReducer from './reducers/userReducer';
 import promiseMiddleware from 'redux-promise-middleware';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
     userReducer
 });
 
-export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+   
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+   
+  export default () => {
+    let store = createStore(persistedReducer, applyMiddleware(promiseMiddleware))
+    let persistor = persistStore(store)
+    return { store, persistor }
+  }
+
+//export default createStore(rootReducer,  applyMiddleware(promiseMiddleware));
