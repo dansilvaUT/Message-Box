c92475982aba5e65caff8269805df5ba9adc9818
dansilvaUT <dan13silv@gmail.com>
2021-04-28 20:57:23 -0700

    got emit message socket working

--- src/Components/Header/Nav/Nav.js
+++ src/Components/Header/Nav/Nav.js
@@ -1,25 +1,30 @@
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { getUser } from '../../../redux/reducers/userReducer';
+import { getUser, clearUser } from '../../../redux/reducers/userReducer';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Nav.scss';
 
 const Nav = props => {
-    const { getUser, username } = props;
+    const { getUser, username, clearUser } = props;
     useEffect(() => {
         axios.get('/api/auth/me')
             .then(user => getUser(user.data))
     }, [getUser]);
 
     // console.log('nav', props)
+    const logout = () => {
+        axios.post('/api/logout')
+            .then(() => clearUser())
+            .catch(err => console.log(`Error: ${err.message}`));
+    }
+    console.log(props)
     return (
         <nav className='navbar'>
             <span>Welcome {username}</span>
             <Link className='link nav-link' to='/dash'>Dashboard</Link>
             <Link className='link nav-link' to='/profile'>Profile</Link>
-            {/* <Link>Messegers</Link>
-                <Link>Log Out</Link> */}
+            <Link to='/' onClick={() => logout()}>Log Out</Link>
         </nav>
     )
 }
@@ -30,4 +35,4 @@ const mapStateToProps = reduxState => {
     }
 }
 
-export default connect(mapStateToProps, { getUser })(Nav);
+export default connect(mapStateToProps, { getUser, clearUser })(Nav);
