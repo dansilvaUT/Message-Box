456e74260d9f41cf86437118df24b95ed044593d
dansilvaUT <dan13silv@gmail.com>
2021-06-05 12:27:03 -0700

    mobile nav included. Need to fix position

--- src/Components/Header/MobileNav/MobileNav.js
+++ src/Components/Header/MobileNav/MobileNav.js
@@ -1,7 +1,41 @@
-const MobileNav = () => {
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import DashboardIcon from '../../Icons/DashboardIcon';
+import { getUser, clearUser } from '../../../redux/reducers/userReducer';
+import LogoutIcon from '../../Icons/LogoutIcon';
+import ProfileIcon from '../../Icons/ProfileIcon';
+import axios from 'axios';
+import './MobileNav.scss';
+
+const MobileNav = props => {
+
+    const { username, getUser, clearUser } = props
+    useEffect(() => {
+        axios.get('/api/auth/me')
+            .then(user => getUser(user.data));
+    }, [getUser]);
+
+    const logout = () => {
+        axios.post('/api/logout')
+            .then(() => clearUser())
+            .catch(err => console.log(`Error: ${err.message}`));
+    }
+    
     return (
-        <></>
+        <nav className='mobile-navbar'>
+            <span id="mobile-header-welcome">Logged In as<span id="header-username"> @{username}</span></span>
+            <Link className='link mobile-nav-link' to='/dash'>Dashboard <DashboardIcon /></Link>
+            <Link className='link mobile-nav-link' to='/profile'>Profile <ProfileIcon /></Link>
+            <Link className='link mobile-nav-link' to='/' onClick={() => logout()}>Log Out <LogoutIcon /></Link>
+        </nav>
     )
 }
 
-export default MobileNav;
+const mapStateToProps = reduxState => {
+    return {
+        username: reduxState.userReducer.user.username
+    }
+}
+
+export default connect(mapStateToProps, { clearUser, getUser })(MobileNav);
--- src/Components/Icons/MenuIcon.js
+++ src/Components/Icons/MenuIcon.js
@@ -1,11 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import MobileNav from '../Header/MobileNav/MobileNav';
 import './IconStyles/IconStyles.scss';
 
 const MenuIcon = () => {
+
+    const [toggle, setToggle] = useState(false);
+
+    const showMenu = () => {
+        setToggle(!toggle);
+    }
+
     return (
         <>
-            <FontAwesomeIcon className="menu-bars" icon={faBars} onClick={()=> alert('Hi')}/>
+            <span className="menu-bars">
+                <FontAwesomeIcon icon={faBars} onClick={() => showMenu()} />
+            </span>
+            {toggle
+                ?
+                <MobileNav />
+                :
+                null
+            }
         </>
     )
 }
